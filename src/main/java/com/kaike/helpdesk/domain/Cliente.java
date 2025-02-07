99d5fd23a574630848a06c695aa4b2363cf34933
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2025-02-07 14:19:53 -0300

    Criando perfil de desenvolvimento

--- src/main/java/com/kaike/helpdesk/domain/Cliente.java
+++ src/main/java/com/kaike/helpdesk/domain/Cliente.java
@@ -1,6 +1,5 @@
 package com.kaike.helpdesk.domain;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
---
+++ src/main/java/com/kaike/helpdesk/services/DBService.java
@@ -0,0 +1,43 @@
+package com.kaike.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kaike.helpdesk.domain.Chamado;
+import com.kaike.helpdesk.domain.Cliente;
+import com.kaike.helpdesk.domain.Tecnico;
+import com.kaike.helpdesk.domain.enums.Perfil;
+import com.kaike.helpdesk.domain.enums.Prioridade;
+import com.kaike.helpdesk.domain.enums.StatusEnum;
+import com.kaike.helpdesk.repositories.ChamadoRepository;
+import com.kaike.helpdesk.repositories.ClienteRepository;
+import com.kaike.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+
+	public void instanciaDB() {
+		Tecnico tec1 = new Tecnico(null, "Kaike Tuerpe", "123456789", "kaike@gmail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+
+		Cliente cli1 = new Cliente(null, "Luysa", "987654321", "luysa@gmail.com", "321");
+
+		Chamado c1 = new Chamado(null, Prioridade.MÉDIA, StatusEnum.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1,
+				cli1);
+
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+}
