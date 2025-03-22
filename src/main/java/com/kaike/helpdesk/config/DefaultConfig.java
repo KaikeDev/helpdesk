2b9cb199179ba126984e6262e7e7b75b9a32deb3
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2025-03-22 15:19:07 -0300

    Criando instâncias no novo banco de dados

--- src/main/java/com/kaike/helpdesk/config/DefaultConfig.java
+++ src/main/java/com/kaike/helpdesk/config/DefaultConfig.java
@@ -1,5 +1,27 @@
 package com.kaike.helpdesk.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.kaike.helpdesk.services.DBService;
+
+@Configuration
 public class DefaultConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+
+    @Bean
+     boolean instanciaDB() {
+		if(value.equals("create")) {
+		this.dbService.instanciaDB();
+		}
+		return false;
+	}
 
 }
